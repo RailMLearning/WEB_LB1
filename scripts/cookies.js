dbfbc2ddba4d655621c87698e889ce3ccbd284db
n3sostr3l <gladv242@gmail.com>
2026-09-08 13:40:41 +0300

    делай валидацию данных формы, мне лень

--- scripts/cookies.js
+++ scripts/cookies.js
@@ -1,19 +1,24 @@
 document.querySelector("#form-modal").
     addEventListener(
         "submit",
-        async () => {
-
+        async (event) => {
+            hideError()
             event.preventDefault();
 
-            let form = document.querySelector("#form-modal")
+            let form = document.querySelector("#studform")
 
             const data = new FormData(form);
-
+            
+            if(!validateId(data.get("isu"))){
+                showError("Неверный ИСУ ID")
+                return false;
+            }
             await cookieStore.set({
                 "isu": data.get("isu"),
-                "fio": data.get("isu"),
-                
+                "fio": data.get("fio"),
+
             })
 
+            closeModal("#form-modal")
         }
     )
--- scripts/utils.js
+++ scripts/utils.js
@@ -1,1 +1,23 @@
-cookieStore = new CookieStore() 
+
+
+function showError(e=""){
+    err = document.getElementById("error")
+    err.innerHTML = e
+    err.classList.add("err-active")
+    err.classList.remove("err-inactive")
+}
+function hideError(){
+    err = document.getElementById("error")
+    err.innerHTML = ""
+    err.classList.add("err-inactive")
+    err.classList.remove("err-active")
+}
+
+function validateId(isu=''){
+    try{
+        parseInt(isu)
+    }catch(e){
+        return false;
+    }
+    return (isu.length == 6)
+}
