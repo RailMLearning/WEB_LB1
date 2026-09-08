37179dee9e5ac8a354370cd1979f881eebd94e6d
n3sostr3l <gladv242@gmail.com>
2026-09-08 15:14:08 +0300

    веселье!

--- scripts/cookies.js
+++ scripts/cookies.js
@@ -8,20 +8,33 @@ document.querySelector("#form-modal").
             let form = document.querySelector("#studform")
 
             const data = new FormData(form);
-            
-            if(!validateId(data.get("isu"))){
-                showError("Неверный ИСУ ID")
+
+
+            //валидация данных формы перед отправкой в куки
+
+            if (!validateId(data.get("isu"))) {
+                showError("Неверный ИСУ ID, введите от 5 до 7 цифр")
                 return false;
             }
+            console.log(data.keys())
+            for (let key of data.keys()) {
 
-            let target 
-            
-            await cookieStore.set({
-                isu: {
-                    "fio": data.get("fio")
-                }
-            })
-
+                await cookieStore.set({
+                    "name": `${key}_isu${data.get("isu")}`,
+                    "value": data.get(key),
+                    expires: Date.now() + 24 * 60 * 60 * 1000, // 1 день
+                    path: '/'
+                })
+            }
             closeModal("#form-modal")
         }
     )
+
+cookieStore.addEventListener(
+    "change",
+    (event) => {
+
+        updateTable()
+
+    }
+)
--- scripts/utils.js
+++ scripts/utils.js
@@ -1,23 +1,93 @@
 
 
-function showError(e=""){
+function showError(e = "") {
     err = document.getElementById("error")
     err.innerHTML = e
     err.classList.add("err-active")
     err.classList.remove("err-inactive")
 }
-function hideError(){
+function hideError() {
     err = document.getElementById("error")
     err.innerHTML = ""
     err.classList.add("err-inactive")
     err.classList.remove("err-active")
 }
 
-function validateId(isu=''){
-    try{
+function validateId(isu = '') {
+    try {
         parseInt(isu)
-    }catch(e){
+    } catch (e) {
         return false;
     }
-    return (isu.length == 6)
+    return (5 <= isu.length <= 7)
+}
+
+function addShortTableRow(data = {
+    "isu": "",
+    "fio": "",
+    "dnum": "",
+    "rnum": ""
+}) {
+    document.getElementById("table-students").
+        innerHTML += `
+        <tr>
+                            <td class="panel">
+                                <button type="button" class="btn btn-edit" id="edit-${data.isu}"
+                                    onclick="openModal('form-modal')">Изменить</button>
+                                <button type="button" class="btn btn-delete" id="delete-${data.isu}"
+                                    onclick="openModal('confirm-modal')">Удалить</button>
+                                <button type="button" class="btn btn-more" id="more-${data.isu}"
+                                    onclick="openModal('more-info-modal')">Подробнее...</button>
+                            </td>
+                            <td>${data.fio}</td>
+                            <td>${data.isu}</td>
+                            <td>${data.dnum}</td>
+                            <td>${data.rnum}</td>
+                        </tr>
+        `
+}
+
+function replaceWideTable(data = {
+    "isu": "",
+    "fio": "",
+    "gr_id": "",
+    "dnum": "",
+    "rnum": "",
+    "foreigner": "",
+    "notes": ""
+}) {
+    document.getElementById("")
+}
+
+async function getStudentById(isu){
+
+    let res = {}
+
+    let cookies = await cookieStore.getAll()
+    cookies.forEach(c => {
+        if(c.name.includes(isu)) res[c.name.split("_")[0]]=c.value
+    })
+
+    return res;
+}
+
+async function getAllIds(){
+    let res = []
+    
+    let cookies = await cookieStore.getAll()
+    cookies.forEach(c => {
+        if(c.name.includes("isu_isu")) res.push(c.name.substring(7))
+    })
+    return res
+}
+
+function updateTables() {
+
+    getAllIds().then(ids => {
+        ids.forEach(id => {
+            getStudentById(id).then(s => {
+                 addShortTableRow(s)
+            })
+        })
+    })
 }
